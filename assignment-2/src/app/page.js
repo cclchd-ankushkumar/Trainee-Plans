da7c6aaede0c0bc8637ae9fc3423be86da2832be
Ankush Kumar <ankush.kumar@chaincodeconsulting.com>
2024-02-19 12:32:23 +0530

    done

--- assignment-2/src/app/page.js
+++ assignment-2/src/app/page.js
@@ -1,113 +1,215 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+import { RotatingLines } from "react-loader-spinner";
+import { apiCalling } from "@/utils/api";
+import AddPage from "@/component/newmodal/AddPage";
+import EditModal from "@/component/edit modal/EditModal";
+import { ClipLoader } from "react-spinners";
 
-export default function Home() {
+const Users = () => {
+  const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [addModal, setAddModal] = useState(false);
+  const addToggle = () => setAddModal(!addModal);
+  const [editModal, setEditModal] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [count, setCount] = useState(0);
+
+  const getData = async () => {
+    const check = await apiCalling(
+      "https://users-data-yzda.onrender.com/user",
+      "GET"
+    );
+    try {
+      setData(check);
+      setLoader(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const editToggle = (id) => {
+    setEditModal(!editModal);
+    setUserId(id);
+  };
+
+  const handleDelete = async (id) => {
+    const Delete = await apiCalling(
+      `https://users-data-yzda.onrender.com/user/${id}`,
+      "DELETE",
+      { "Content-type": "application/json" }
+    );
+    alert(`User with Id ${id} deleted successfully!`);
+    console.log(Delete);
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [data]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div>
+      <nav className="w-full h-auto bg-gradient-to-r from-blue-800 to-blue-500 py-3 flex">
+        <div className="w-3/5 h-full sm:pl-10 pl-3">
+          <h1 className="text-white sm:text-3xl text-2xl font-bold">
+            Assignment-2
+          </h1>
+        </div>
+
+        <div className="w-2/5 text-white h-auto pr-5 flex justify-end">
+          <div className="w-24 h-full flex items-center justify-around">
+            <p>Search </p>
+            <p>
+              <FaSearch />
+            </p>
+          </div>
+        </div>
+      </nav>
+
+      <div className="flex justify-between">
+        <div className="lg:block hidden w-1/6 h-screen bg-white px-2 custom-box-shadow">
+          <div className="w-full h-auto relative">
+            <button
+              className="w-full px-6 py-2 absolute top-24 text-white bg-gradient-to-r from-blue-800 to-blue-200  rounded-sm box-border hover:text-blue-800 font-semibold"
+              onClick={addToggle}
+            >
+              Add User
+            </button>
+          </div>
+        </div>
+
+        {/* dashboard data */}
+        <div className="lg:w-5/6 w-full">
+          <div className="w-full h-auto mt-10 items-center flex justify-around  flex-wrap m-auto">
+            <div className="w-60 h-32 mb-6 mr-5 p-5 text-center rounded-md bg-gradient-to-r from-blue-800 to-blue-200 relative">
+              <h2 className="font-bold text-xl text-white mb-3">Total Users</h2>
+              <h1 className="font-bold text-3xl text-white">
+                {!loader ? (
+                  <div className="absolute left-1/2 -translate-x-1/2 top-3/5">
+                    <ClipLoader color="white" />
+                  </div>
+                ) : (
+                  234
+                )}
+              </h1>
+            </div>
+            <div className="w-60 h-32 mb-6 mr-5 rounded-md text-center p-5 bg-gradient-to-r from-blue-800 to-blue-200">
+              <h2 className="font-bold text-xl text-white mb-3">
+                Active Users
+              </h2>
+              <h1 className="font-bold text-3xl text-white">
+                {!loader ? (
+                  <ClipLoader color="white" />
+                ) : data.length < 10 ? (
+                  "0" + data.length
+                ) : (
+                  data.length
+                )}
+              </h1>
+            </div>
+            <div className="w-60 h-32 mb-6 mr-5 p-5 rounded-md bg-gradient-to-r from-blue-800 to-blue-200 text-center">
+              <h2 className="font-bold text-xl text-white mb-3">
+                Deleted Users
+              </h2>
+              <h1 className="font-bold text-3xl text-white">
+                {!loader ? (
+                  <ClipLoader color="white" />
+                ) : count < 10 ? (
+                  "0" + count
+                ) : (
+                  count
+                )}
+                {}
+              </h1>
+            </div>
+          </div>
+
+          {addModal ? (
+            <AddPage addModal={addModal} addToggle={addToggle} />
+          ) : null}
+          {editModal ? (
+            <EditModal
+              userId={userId}
+              editModal={editModal}
+              editToggle={editToggle}
             />
-          </a>
+          ) : null}
+
+          <div className=" w-11/12 text-center m-auto mt-6 flex justify-around">
+            <h1 className="sm:text-3xl text-3xl text-blue-800 font-semibold">
+              All Users
+            </h1>
+            <button
+              className="bg-blue-800 px-6 py-2 text-white rounded-md lg:hidden block"
+              onClick={addToggle}
+            >
+              Add User
+            </button>
+          </div>
+
+          <div className="flex overflow-x-auto whitespace-nowrap mt-10">
+            {!loader ? (
+              <div className="absolute left-1/2 -translate-x-1/2">
+                <RotatingLines
+                  visible={true}
+                  height="96"
+                  width="96"
+                  color={"failure"}
+                  strokeWidth="5"
+                  animationDuration="0.75"
+                  ariaLabel="rotating-lines-loading"
+                  wrapperStyle={{ color: "blue" }}
+                  wrapperClass="dashboard_custom_spinner"
+                />
+              </div>
+            ) : (
+              <table className=" m-auto ">
+                <thead>
+                  <tr className="border-b border-gray-300">
+                    <th className="py-4 px-8  text-left">S no.</th>
+                    <th className="py-4 px-8  text-left">Name</th>
+                    <th className="py-4 px-8  text-left">Age</th>
+                    <th className="py-4 px-8  text-left">Email</th>
+                    <th className="py-4 px-8  text-left">Gender</th>
+                    <th className="py-4 px-8  text-left">Phone</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {data?.map((ele, index) => (
+                    <tr key={ele.id} className="border-b border-gray-300">
+                      <td className="py-4 px-8  text-left">{index + 1}</td>
+                      <td className="py-4 px-8 text-left">{ele.firstName}</td>
+                      <td className="py-4 px-8 text-left">{ele.age}</td>
+                      <td className="py-4 px-8 text-left">{ele.email}</td>
+                      <td className="py-4 px-8 text-left">{ele.gender}</td>
+                      <td className="py-4 px-8 text-left">{ele.phone}</td>
+                      <td className="pl-5 pr-3">
+                        <button
+                          onClick={() => handleDelete(ele.id)}
+                          className="text-red-600 text-2xl"
+                        >
+                          <MdDelete />
+                        </button>
+                      </td>
+                      <td className=" px-3">
+                        <button onClick={() => editToggle(ele.id)}>
+                          <FaEdit />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Users;
